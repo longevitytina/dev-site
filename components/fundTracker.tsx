365d583b2f5c8fa6f83ac20547a4e5a69a78ffc7
Tina Taylor <longevity.tina@gmail.com>
2025-03-14 15:53:29 -0700

    fix: update icon

--- components/fundTracker.tsx
+++ components/fundTracker.tsx
@@ -13,22 +13,42 @@ interface FundTrackerProps {
   linkText?: string;
 }
 
-const FundTracker = ({ fundName, fundGoal, fundCurrent, description, link, linkText }: FundTrackerProps) => {
+const FundTracker = ({
+  fundName,
+  fundGoal,
+  fundCurrent,
+  description,
+  link,
+  linkText,
+}: FundTrackerProps) => {
   return (
     <div className='max-w-[500px] border-2 border-gray-300 border-dashed p-2'>
       <h1 className='flex flex-row items-center gap-2'>
         <Image
-          src='/Diaper.svg'
+          src='/diaper-svgrepo-com.svg'
           alt={fundName}
           width={20}
           height={20}
+          className='w-10 h-10 my-2'
         />
-        {fundName} {' '}
-        Fund Tracker
+        {fundName} Fund Tracker
       </h1>
-      <TrackerBar fundGoal={fundGoal} fundCurrent={fundCurrent} />
+      <TrackerBar
+        fundGoal={fundGoal}
+        fundCurrent={fundCurrent}
+      />
       {description && <p>{description}</p>}
-      Read More:{link && <a className='text-blue-500 underline' href={link} target="_blank" rel="noopener noreferrer">{linkText}</a>}
+      Read More:
+      {link && (
+        <a
+          className='text-blue-500 underline'
+          href={link}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          {linkText}
+        </a>
+      )}
       <br />
       <Payment fundName={fundName} />
     </div>
